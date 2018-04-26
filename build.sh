af9e102fa78258e8b00db4dcf413d637bcf70a14
Michael Tiller <michael.tiller@gmail.com>
2018-04-26 17:14:49 -0400

    Removed unnecessary file

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wget https://github.com/ORNL/TASMANIAN/archive/v5.1.tar.gz && tar zxf TASMANIAN-5.1.tar.gz
-(cd TASMANIAN-5.1; echo -e "y\nn" | ./install /usr/local/tasmanian-5.1/)
-rm -rf TASMANIAN-*
